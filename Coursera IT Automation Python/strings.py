cf4c1371fa84196e03e86f8dc83401b859633e47
dimon <dimazill@aol.com>
2023-12-24 21:51:46 -0800

    latest changes

--- Coursera IT Automation Python/strings.py
+++ Coursera IT Automation Python/strings.py
@@ -6,7 +6,7 @@ print(name[6:])
 print(name[6:])
 print(name[:5])
 print(name.index("J"))
-<<<<<<< HEAD
+
 # 0
 # print(name.index("q"))
 # error
@@ -50,14 +50,14 @@ def count_letters(text):
 
 print(count_letters("AaBbCc"))
 # Should be {'a': 2, 'b': 2, 'c': 2}
-
+#
 print(count_letters("Math is fun! 2+2=4"))
 # Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
 print(count_letters("This is a sentence."))
 # Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
-=======
+#
 #0
 #print(name.index("q"))
 #error
 print("q" in name)
->>>>>>> origin/master
+
